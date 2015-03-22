3a7a6e63cb505039722893d5a1661ce064b482b9
Richard Leung <lyskr@126.com>
2015-03-22 15:26:03 +0800

    Initial Commit

---
+++ RLPickerDoubleView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RLPickerDoubleView
+//
+//  Created by 梁原 on 15/3/22.
+//  Copyright (c) 2015年 RL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RLPickerDoubleView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RLPickerDoubleView
+//
+//  Created by 梁原 on 15/3/22.
+//  Copyright (c) 2015年 RL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
